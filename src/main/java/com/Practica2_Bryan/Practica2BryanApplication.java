00565ca21db53915214f579eae65b2c868d10561
vinda <vinda@DESKTOP-2FJ4FER>
2022-10-10 17:45:04 -0600

    Practica #2 Semana5

---
+++ src/main/java/com/Practica2_Bryan/Practica2BryanApplication.java
@@ -0,0 +1,13 @@
+package com.Practica2_Bryan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2BryanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2BryanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Practica2_Bryan/Practica2BryanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practica2_Bryan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica2BryanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
